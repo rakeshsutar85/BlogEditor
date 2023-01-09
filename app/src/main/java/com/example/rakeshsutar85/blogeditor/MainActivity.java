9aab1689ae7e9dba801d70a4f071efdb2add8642
rakesh <rakeshsutar85@gmail.com>
2023-01-09 10:56:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/rakeshsutar85/blogeditor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rakeshsutar85.blogeditor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
